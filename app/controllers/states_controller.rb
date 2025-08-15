2be82daab7ea74b8445913017d8056d4e6c2fc44
Junfeng Ou <jun@Junfengs-MacBook-Pro.local>
2025-08-15 12:59:26 -0500

    Update the states controller

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,32 +1,30 @@
 class StatesController < ApplicationController
-    before_action :set_service
-    before_action :set_state, only: [:show, :update]
-    
-    # GET /states
-    def index
-      render json: @service.list_states
-    end
-  
-    # GET /states/:id
-    def show
-      state = @service.get_state(params[:id])
-      state ? render(json: state) : head(:not_found)
-    end
-  
-    # PUT /states/:id
-    def update
-      state = @service.update_state(params[:id], state_params)
-      state ? render(json: state) : head(:not_found)
-    end
-  
-    private
-  
-    def set_service
-      @service = StateService.new
-    end
-  
-    def state_params
-      params.require(:state).permit(:name, :abbreviation)
-    end
+  before_action :set_service
+
+  # GET /states
+  def index
+    render json: @service.list_states
+  end
+
+  # GET /states/:id
+  def show
+    state = @service.get_state(params[:id])
+    state ? render(json: state) : head(:not_found)
+  end
+
+  # PUT /states/:id
+  def update
+    state = @service.update_state(params[:id], state_params)
+    state ? render(json: state) : head(:not_found)
+  end
+
+  private
+
+  def set_service
+    @service = StateService.new
+  end
+
+  def state_params
+    params.require(:state).permit(:name, :abbreviation)
+  end
 end
-  
